35b027cf69f9c8bf5082c5caafb6bfa3aeb7cd8c
thiyagu <thiyagu@programmer.net>
2020-07-20 14:52:30 +0530

    API partitions based on UAT and PROD

--- library/src/main/java/com/paysez/library/AppConfig.java
+++ library/src/main/java/com/paysez/library/AppConfig.java
@@ -2,10 +2,10 @@ package com.paysez.library;
 public class AppConfig {
     // Prod
 
-    public static String baseurl = "https://pg.credopay.in";
-    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
-    public  static String cardSubmit = "https://pg.credopay.in/credopay/api/visasubmit1.php";
-    public static String RupayLeg = "https://pg.credopay.in/credopay/api/CPDirectPG.php";
+//    public static String baseurl = "https://pg.credopay.in";
+//    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
+//    public  static String cardSubmit = "https://pg.credopay.in/credopay/api/visasubmit1.php";
+//    public static String RupayLeg = "https://pg.credopay.in/credopay/api/CPDirectPG.php";
 
 
     //Prod
@@ -18,10 +18,10 @@ public class AppConfig {
     //UAT
 
 
-//    public static String baseurl= "https://pguat.credopay.info";
-//    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
-//    public  static String cardSubmit = "https://pguat.credopay.info/credopay/api/visasubmit1.php";
-//    public static String RupayLeg = "https://pguat.credopay.info/credopay/api/CPDirectPG.php";
+    public static String baseurl= "https://pguat.credopay.info";
+    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
+    public  static String cardSubmit = "https://pguat.credopay.info/credopay/api/visasubmit1.php";
+    public static String RupayLeg = "https://pguat.credopay.info/credopay/api/CPDirectPG.php";
 
     //UAT
 }
