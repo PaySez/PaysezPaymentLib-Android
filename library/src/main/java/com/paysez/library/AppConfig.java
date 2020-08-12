ad6e8200e953a799ab4a73495e22d7be5d691825
Thiyagu <thiyagu@programmer.net>
2020-08-12 18:12:57 +0530

    Reversed 1.0

--- library/src/main/java/com/paysez/library/AppConfig.java
+++ library/src/main/java/com/paysez/library/AppConfig.java
@@ -4,27 +4,27 @@ public class AppConfig {
 
 
     // Prod
-    public static String sdk_version = "prod-1.0";
-    public static String baseurl = "https://pg.credopay.in";
-    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
-    public static String cardSubmit = "https://pg.credopay.in/credopay/api/visasubmit1.php";
-    public static String RupayLeg = "https://pg.credopay.in/credopay/api/CPDirectPG.php";
-    public static String returnUrl = "https://pg.credopay.in/credopay/api/appresponsemerchant.php?randomgen=";
-    public static String netbanking_sale = "https://pg.credopay.in/credopay/CPWebPG.php";
-    public static String netbanking_query = "https://pg.credopay.in/credopaylogin/NBQuery_status.php";
+//    public static String sdk_version = "prod-1.0";
+//    public static String baseurl = "https://pg.credopay.in";
+//    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
+//    public static String cardSubmit = "https://pg.credopay.in/credopay/api/visasubmit1.php";
+//    public static String RupayLeg = "https://pg.credopay.in/credopay/api/CPDirectPG.php";
+//    public static String returnUrl = "https://pg.credopay.in/credopay/api/appresponsemerchant.php?randomgen=";
+//    public static String netbanking_sale = "https://pg.credopay.in/credopay/CPWebPG.php";
+//    public static String netbanking_query = "https://pg.credopay.in/credopaylogin/NBQuery_status.php";
 
 
     //UAT
 
 
-//    public static String sdk_version = "uat-1.0";
-//    public static String baseurl = "https://pguat.credopay.info";
-//    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
-//    public static String cardSubmit = "https://pguat.credopay.info/credopay/api/visasubmit1.php";
-//    public static String RupayLeg = "https://pguat.credopay.info/credopay/api/CPDirectPG.php";
-//    public static String returnUrl = "https://pguat.credopay.info/credopay/api/appresponsemerchant.php?randomgen=";
-//    public static String netbanking_sale = "https://pguat.credopay.info/credopay/CPWebPG.php";
-//    public static String netbanking_query = "https://pguat.credopay.info/credopaylogin/NBQuery_status.php";
+    public static String sdk_version = "uat-1.0";
+    public static String baseurl = "https://pguat.credopay.info";
+    public static String ENDPOINT_FIRSTLEG_REQUEST = "/credopay/api/CPDirectPG.php";
+    public static String cardSubmit = "https://pguat.credopay.info/credopay/api/visasubmit1.php";
+    public static String RupayLeg = "https://pguat.credopay.info/credopay/api/CPDirectPG.php";
+    public static String returnUrl = "https://pguat.credopay.info/credopay/api/appresponsemerchant.php?randomgen=";
+    public static String netbanking_sale = "https://pguat.credopay.info/credopay/CPWebPG.php";
+    public static String netbanking_query = "https://pguat.credopay.info/credopaylogin/NBQuery_status.php";
 
 
 //local debug
